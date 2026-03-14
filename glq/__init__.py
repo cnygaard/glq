04909af7f34fd9d666ee720bf63cb22de9787bfd
Christian Nygaard <christian@cnygaard.com>
2026-03-14 20:47:54 +0100

    Bump version to 0.1.7

--- glq/__init__.py
+++ glq/__init__.py
@@ -5,7 +5,7 @@ E8 lattice shell codebook + Randomized Hadamard Transform (RHT)
 for 2-bit post-training quantization of LLM weights.
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 from .codebook import E8ShellCodebook
 from .hadamard import fast_hadamard_transform
